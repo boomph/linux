b90f2e27ed13c7e406666ba09736c278df68c993
rr <2419725841@qq.com>
2021-03-17 07:33:20 +0800

    bug: main.c pb3未初始化

--- V3S/licheepi_zero/Board_Drivers/5_ledc_bsp/project/main.c
+++ V3S/licheepi_zero/Board_Drivers/5_ledc_bsp/project/main.c
@@ -6,10 +6,13 @@ int main(void){
 
     //gpio pb2,pb3 mode = out
     GPIO_Init(PIO_PORT_B,2,PIO_MODE_OUT,PIO_DRV_LEVEL1,PIO_PULL_DISABLE);
+    GPIO_Init(PIO_PORT_B,3,PIO_MODE_OUT,PIO_DRV_LEVEL1,PIO_PULL_DISABLE);
+
+
 
     //PB2 = LOW
     GPIO_ResetPin(PIO_PORT_B,2);
-
+    
     //PB3 = HIGH
     GPIO_SetPin(PIO_PORT_B,3);
 
