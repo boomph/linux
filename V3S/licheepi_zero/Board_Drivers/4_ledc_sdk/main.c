90290800f54b0d93dd1b63e070023095ae455d9e
rr <2419725841@qq.com>
2021-03-16 10:00:55 +0800

    4_ledc_sdk  有链接脚本，未测试

---
+++ V3S/licheepi_zero/Board_Drivers/4_ledc_sdk/main.c
@@ -0,0 +1,22 @@
+#include "v3s_sys.h"
+#include "v3s_gpio.h"
+
+
+int main(void){
+
+    //gpio pb2,pb3 mode = out
+    GPIO_Init(PIO_PORT_B,2,PIO_MODE_OUT,PIO_DRV_LEVEL1,PIO_PULL_DISABLE);
+
+    //PB2 = LOW
+    GPIO_ResetPin(PIO_PORT_B,2);
+
+    //PB3 = HIGH
+    GPIO_SetPin(PIO_PORT_B,3);
+
+    while(1){
+
+    }
+
+    return 0;
+
+}
