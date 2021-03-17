dc174553efcdf91eb5910fa7f160a2f0dfda4a34
b <2419725841@qq.com>
2021-03-17 17:05:38 +0800

    PB2 输入 下拉 LEVEL1

--- V3S/licheepi_zero/Board_Drivers/7_key/project/main.c
+++ V3S/licheepi_zero/Board_Drivers/7_key/project/main.c
@@ -4,7 +4,7 @@
 
 int main(void){
     //init pb2 上拉，作为KEY触发口
-    GPIO_Init(PIO_PORT_B,2,PIO_MODE_IN,PIO_DRV_LEVEL1,PIO_PULL_DISABLE);
+    GPIO_Init(PIO_PORT_B,2,PIO_MODE_IN,PIO_DRV_LEVEL1,PIO_PULL_DOWN);
 
     //init pb3 输出，当KEY触发时，PB3输出高电平，点亮LED
     GPIO_Init(PIO_PORT_B,3,PIO_MODE_OUT,PIO_DRV_LEVEL1,PIO_PULL_DISABLE);
@@ -13,10 +13,10 @@ int main(void){
     GPIO_ResetPin(PIO_PORT_B,3);
 
     while(1){
-        if(GPIO_GetPinData(PIO_PORT_B,2) == 0)
-            GPIO_SetPin(PIO_PORT_B,3);
-	else
-	    GPIO_ResetPin(PIO_PORT_B,3);
+//        if(GPIO_GetPinData(PIO_PORT_B,2) == 0)
+  //          GPIO_SetPin(PIO_PORT_B,3);
+//	else
+//	    GPIO_ResetPin(PIO_PORT_B,3);
     }
 
     return 0;
