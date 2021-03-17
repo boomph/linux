2dd6bc8f17d2e91aad1b5aaaddc1bb64ace6ccaf
b <2419725841@qq.com>
2021-03-17 12:27:07 +0800

    PB2输入模式，禁止上拉

--- V3S/licheepi_zero/Board_Drivers/7_key/project/main.c
+++ V3S/licheepi_zero/Board_Drivers/7_key/project/main.c
@@ -4,7 +4,7 @@
 
 int main(void){
     //init pb2 上拉，作为KEY触发口
-    GPIO_Init(PIO_PORT_B,2,PIO_MODE_IN,PIO_DRV_LEVEL0,PIO_PULL_UP);
+    GPIO_Init(PIO_PORT_B,2,PIO_MODE_IN,PIO_DRV_LEVEL1,PIO_PULL_DISABLE);
 
     //init pb3 输出，当KEY触发时，PB3输出高电平，点亮LED
     GPIO_Init(PIO_PORT_B,3,PIO_MODE_OUT,PIO_DRV_LEVEL1,PIO_PULL_DISABLE);
@@ -15,6 +15,8 @@ int main(void){
     while(1){
         if(GPIO_GetPinData(PIO_PORT_B,2) == 0)
             GPIO_SetPin(PIO_PORT_B,3);
+	else
+	    GPIO_ResetPin(PIO_PORT_B,3);
     }
 
     return 0;
